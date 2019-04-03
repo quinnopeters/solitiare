5ec05346b73eac6e90644a322e7363d99d9c9e04
AmeliaShields <the.amelia.s@gmail.com>
2019-04-03 11:16:44 -0700

    Cards

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,65 @@
+﻿
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+
+	public enum Suit
+	{
+		diamond,
+		club,
+		heart,
+		spade
+	}
+
+	public enum Rank
+	{
+		ace = 1,
+		two = 2,
+		three = 3,
+		four = 4,
+		five = 5,
+		six = 6,
+		seven = 7,
+		eight = 8,
+		nine = 9,
+		ten = 10,
+		jack = 11,
+		queen = 12,
+		king = 13
+	}
+
+
+
+
+
+	private Suit cardSuit;
+	private Rank cardRank;
+
+
+	public Card(Suit suit, Rank rank)
+	{
+		cardSuit = suit;
+		cardRank = rank;
+	}
+
+	public Rank getCardRank()
+	{
+		return cardRank;
+	}
+
+	public Suit getCardSuit()
+	{
+		return cardSuit;
+	}
+
+	public void setCardRank(Rank cardRank)
+	{
+		this.cardRank = cardRank;
+	}
+
+	public void setCardSuit(Suit cardSuit)
+	{
+		this.cardSuit = cardSuit;
+	}
+}
--- Assets/Scripts/CardCollection.cs
+++ Assets/Scripts/CardCollection.cs
@@ -4,13 +4,7 @@ using UnityEngine;
 
 public abstract class CardCollection : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
 		
-	}
+	
 }
